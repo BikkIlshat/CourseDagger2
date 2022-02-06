fec7409fd125166fc32baef7c2ca6dfd23a01d42
BikkIlshat <HeavyIlshat@rambler.ru>
2022-02-06 15:36:18 +0300

    IntoMap - аналогична IntoSet, но собирает объекты не в Set, а в Map.

--- app/src/main/java/com/github/bikkilshat/coursedagger_2/di/AppComponent.kt
+++ app/src/main/java/com/github/bikkilshat/coursedagger_2/di/AppComponent.kt
@@ -1,17 +1,18 @@
 package com.github.bikkilshat.coursedagger_2.di
 
+import com.github.bikkilshat.coursedagger_2.intoMap.EventHandlerIntoMap
+import com.github.bikkilshat.coursedagger_2.intoMap.EventHandlerType
+import com.github.bikkilshat.coursedagger_2.intoMap.EventModuleIntoMap
 import com.github.bikkilshat.coursedagger_2.intoSet.EvenModule
 import com.github.bikkilshat.coursedagger_2.intoSet.EventHandler
 import com.github.bikkilshat.coursedagger_2.network.ServerApi
 import dagger.Component
-import dagger.multibindings.ElementsIntoSet
-import dagger.multibindings.IntoSet
 
 /***
 Совокупность всех объектов, которые умеет создавать компонент - это граф объектов компонента или граф зависимостей компонента.
  */
 
-@Component(modules = [StorageModule::class, NetworkModule::class, MainModule::class, EvenModule::class])
+@Component(modules = [StorageModule::class, NetworkModule::class, MainModule::class, EvenModule::class, EventModuleIntoMap::class])
 interface AppComponent {
 
   /* аннотации @Qualifier
@@ -22,15 +23,27 @@ interface AppComponent {
   @NetworkModule.Prod("2") //  сказали компоненту вернуть ServerApi с Qualifier Prod
   fun getServerApiProd(): ServerApi
 
-
-
-  //IntoSet
-  // Теперь даггер сможет собрать коллекцию Set<EventHandler>, состоящую из двух  объектов:
-  //fun provideAnalytics(): EventHandler
-  // fun provideLogger(): EventHandler
-
+  /*IntoSet
+  Теперь даггер сможет собрать коллекцию Set<EventHandler>, состоящую из двух  объектов:
+  fun provideAnalytics(): EventHandler
+  fun provideLogger(): EventHandler
+   */
 
   fun getEventHandlers(): Set<EventHandler>
+
+
+
+  /*IntoMap
+  Теперь даггер сможет собрать коллекцию Set<EventHandler>, состоящую из двух  объектов:
+  fun provideAnalytics(): EventHandlerIntoMap
+  fun provideLogger(): EventHandlerIntoMap
+   */
+
+//  fun getEventHandlersIntoMap(): Map<String, EventHandlerIntoMap> пример с ключом String
+
+
+  //пример когда создали свою аннотацию EventHandlerKey с помощью MapKey
+  fun getEventHandlersIntoMap(): Map<EventHandlerType, EventHandlerIntoMap>
 
 }
 
--- app/src/main/java/com/github/bikkilshat/coursedagger_2/intoSet/EventHandler.kt
+++ app/src/main/java/com/github/bikkilshat/coursedagger_2/intoSet/EventHandler.kt
@@ -1,9 +1,7 @@
 package com.github.bikkilshat.coursedagger_2.intoSet
 
-import com.github.bikkilshat.coursedagger_2.storage.DatabaseHelper
 import dagger.Module
 import dagger.Provides
-import dagger.multibindings.ElementsIntoSet
 import dagger.multibindings.IntoSet
 
 /**
