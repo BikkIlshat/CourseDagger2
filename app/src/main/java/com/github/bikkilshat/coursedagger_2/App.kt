afc9150606598f8e883a6ff53349dd029fce27ed
BikkIlshat <HeavyIlshat@rambler.ru>
2022-02-07 19:45:58 +0300

    @Component.Factory

--- app/src/main/java/com/github/bikkilshat/coursedagger_2/App.kt
+++ app/src/main/java/com/github/bikkilshat/coursedagger_2/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.github.bikkilshat.coursedagger_2.di.AppComponent
 import com.github.bikkilshat.coursedagger_2.di.AppModule
 import com.github.bikkilshat.coursedagger_2.di.DaggerAppComponent
+import com.github.bikkilshat.coursedagger_2.di.NetworkModule
 
 
 /***
@@ -17,8 +18,7 @@ class App: Application() {
   override fun onCreate() {
     super.onCreate()
     appComponent = DaggerAppComponent
-      .builder() //Метод builder() теперь возвращает наш кастомный билдер.
-      .context(this) //Код вызывается в Application классе, поэтому this - это Context
-      .buildAppComp() //И мы вызываем у него метод buildAppComp, чтобы получить компонент.
+      .factory()
+      .create(this, NetworkModule())
   }
 }
